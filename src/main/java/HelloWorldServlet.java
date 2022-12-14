9165dd8e968c6e58ccf57515f08ac978a5ac5d26
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-12-14 14:48:01 -0600

    servlet ex complete

---
+++ src/main/java/HelloWorldServlet.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        String name = request.getParameter("name");
+        PrintWriter out = response.getWriter();
+        String cust = name == null || name.isEmpty()?"World!":name;
+        out.printf("<h1>Hello %s</h1>", cust);
+        out.println("<h1>Hello, World!</h1>");
+        out.println("<h2>Hey there</h2>");
+    }
+}
