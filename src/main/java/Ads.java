baf10513209e2f21d3ec5a6f0d9ba5fbf2df4619
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2023-01-05 10:30:20 -0600

    jdbc complete

--- src/main/java/Ads.java
+++ src/main/java/Ads.java
@@ -1,8 +1,9 @@
 import java.util.List;
+import java.sql.SQLException;
 
 public interface Ads {
     // get a list of all the ads
-    List<Ad> all();
+    List<Ad> all() throws SQLException;
     // insert a new ad and return the new ad's id
-    Long insert(Ad ad);
+    Long insert(Ad ad) throws SQLException;
 }
